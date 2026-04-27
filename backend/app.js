02a69bcb67610e9537aea4f0d1438e9d23c84fbf
userName <userEmail>
2026-04-27 19:50:56 +0800

    feat:complete

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
  * @Author: '超绝大帅哥' '3425395584@qq.com'
  * @Date: 2026-01-18 15:08:52
  * @LastEditors: userName userEmail
- * @LastEditTime: 2026-04-27 12:21:11
+ * @LastEditTime: 2026-04-27 18:53:15
  * @FilePath: \徐晨冰_Node_20260118\第四十八天\express-login\app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -47,7 +47,7 @@ const createApp = async () => {
 
   const server = http.createServer(app)
   
-  server.listen(3000, () => {
+  server.listen(3000, "0.0.0.0", () => {
     console.log(`listen 8080 port`)
   });
 
--- backend/router/routes.js
+++ backend/router/routes.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: userName userEmail
+ * @Date: 2026-04-26 21:08:32
+ * @LastEditors: userName userEmail
+ * @LastEditTime: 2026-04-27 19:30:17
+ * @FilePath: \vue-blog\backend\router\routes.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 const { busRouter } = require("./modules/busRouter");
 const {pubKeyRouter} = require("./modules/pubKeyRouter");
